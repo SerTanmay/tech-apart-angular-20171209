38b9aa684c169653fecfdfbf15fb686c3b6e0661
Tanmay Pereira Naik <tanmay36rocks@gmail.com>
2017-12-09 14:06:07 +0530

    Added Trip interface

--- fly-with-angular/src/app/app.component.ts
+++ fly-with-angular/src/app/app.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
-
+import { Trip } from './models/trip';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
-  trip-name = 'Super Flights to Mars';
+  //title = 'app';
+  trip: Trip = {
+    name: 'Super Flights to Mars';
+    price: 18200;
+    duration: '2 Earth Years';
+    description: 'Race through the Solar System to reach the Red planet. This flight will take 11 Earth Years for each side of the journey.';
+  };
+  
 }
---
+++ fly-with-angular/src/app/models/trip.ts
@@ -0,0 +1,6 @@
+export interface Trip{
+    name: string;
+    price: number;
+    duration: string;
+    description: string;
+}
